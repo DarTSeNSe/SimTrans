d799706875288ba52dc9cac461cd82191df7ee8f
Alex SeNSe <sense8life@gmail.com>
2011-12-08 17:40:57 +0200

    +factory, gravatar

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                  "Alex Sense"
+  user.email                 "sense@example.com"
+  user.password              "Sirinity"
+  user.password_confirmation "Sirinity"
+end
