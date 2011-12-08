3326117eaf5226a2cb94077f58e387c488fd7274
Alex SeNSe <sense8life@gmail.com>
2011-12-08 13:04:51 +0200

    +views, routs, tests for pages_cont

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def title
+    base_title = "Simple translation service"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
