8860d5993469208fb34c573c7d6e050063496a6f
Alex SeNSe <sense8life@gmail.com>
2011-12-08 09:51:55 +0200

    SimTrans -T +mysql,rspec,gemfile,gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simtrans::Application.config.secret_token = '11842d20948ebc9da492acb52d71c8f127123cdb030e7adf01dcc97e537a3a3a2e30a49a15da351c2c83a031a18c44622931f635270c61dd1c1e85ddd561d91a'
